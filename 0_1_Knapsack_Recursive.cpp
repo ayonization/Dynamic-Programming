4a57e8cae1fad13d1268a62c0cebc203b65c4101
Ayon Sen <ayon.s2000@gmail.com>
2021-04-07 16:36:53 +0530

    Knapsack Recursive

--- 0_1_Knapsack_Recursive.cpp
+++ 0_1_Knapsack_Recursive.cpp
@@ -1,6 +1,40 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+int knapsack(int wt[], int val[], int w, int n)
+{
+    if (n == 0 or w == 0)
+        return 0;
+    if (wt[n - 1] > w)
+        return knapsack(wt, val, w, n - 1);
+    if (wt[n - 1] <= w)
+        return max(knapsack(wt, val, w, n - 1), val[n - 1] + knapsack(wt, val, w - wt[n - 1], n - 1));
+}
+
 int main()
 {
-    cout<<"Second"<<endl;
+    int n;
+    cout << "Enter size of array " << endl;
+    cin >> n;
+
+    int wt[n];
+    cout << "Enter weight array " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> wt[i];
+    }
+
+    int val[n];
+    cout << "Enter value array" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> val[i];
+    }
+
+    int w;
+    cout << "Enter max weight" << endl;
+    cin >> w;
+
+    int ans = knapsack(wt, val, w, n);
+
+    cout << "Answer is " << ans << endl;
 }
