a3a837939e90b14073f49498d246ebb58bf454c0
Ayon Sen <ayon.s2000@gmail.com>
2021-04-23 19:12:15 +0530

    LCS Top Down

---
+++ LCS_Top_Down.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+int dp[1000][1000];
+int lcs(string s1,string s2,int n,int m)
+{
+    for (int i = 0; i <=n; i++)
+    {
+        for (int j = 0; j <=m; j++)
+        {
+            if(i==0 or j==0) dp[i][j]=0;
+            else
+            {
+                if(s1[i-1]==s2[j-1]) dp[i][j]=1+dp[i-1][j-1];
+                else dp[i][j]= max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        
+    }
+    return dp[n][m];
+    
+}
+int main()
+{   
+    memset(dp,-1,sizeof(dp));
+    string s1,s2;
+    cout<<"Enter first string"<<endl;
+    cin>>s1;
+    cout<<"Enter second string"<<endl;
+    cin>>s2;
+    int n=s1.length();
+    int m=s2.length();
+    int ans=lcs(s1,s2,n,m);
+    cout<<"Length of longest common subsequence is "<<ans<<endl;
+}
