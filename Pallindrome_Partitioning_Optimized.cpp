fb770c8d777935f2df652074b121c4f8c82be5be
Ayon Sen <ayon.s2000@gmail.com>
2021-05-03 16:33:17 +0530

    Pallindrome Partitioning Optimized

---
+++ Pallindrome_Partitioning_Optimized.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+int dp[1000][1000];
+bool ispallindrome(string s,int start,int end)
+{
+        bool ans=true;
+
+        int sum=start+end;
+
+        for (int i = start; i <=(start+end)/2 ; i++)
+        {
+            if(s[i]!=s[sum-i]) ans=false;
+        }
+
+        return ans;
+        
+
+}   
+
+
+int partition(string s, int i,int j)
+{   
+    if(i>=j) return 0;
+
+    if(dp[i][j]!=-1) return dp[i][j];
+
+    if(ispallindrome(s,i,j)) return dp[i][j]=0;
+
+    int ans=INT_MAX;
+
+    for (int k = i; k <=j-1 ; k++)
+    {
+        int temp=1;
+        if(dp[i][k]!=-1) temp+=dp[i][k];
+        else temp+=partition(s,i,k);
+        if(dp[k+1][j]!=-1) temp+=dp[k+1][j];
+        else temp+=partition(s,k+1,j);
+        ans=min(ans,temp);
+    }
+
+    return dp[i][j]=ans;
+    
+}
+int main()
+{   
+    memset(dp,-1,sizeof(dp));
+    string s;
+    cout<<"Enter string"<<endl;
+    cin>>s;
+
+    int ansmemo=partition(s,0,s.length()-1);
+
+    cout<<"Minimum number of partitons for pallindromic substrings is "<<ansmemo<<endl;
+
+
+
+}
