cf582ffce6f5bf6f8c9525d245a7434a4894cfd3
Ayon Sen <ayon.s2000@gmail.com>
2021-05-04 19:50:43 +0530

    Evaluate Boolean Expression Memoized

--- Evalutate_Boolean_Expression_Memiozed.cpp
+++ Evalutate_Boolean_Expression_Memiozed.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-int dp[1000][1000];
+unordered_map<string,int> mp;
 
 int evaluate(string s,int i,int j,bool isTrue)
 {
@@ -11,7 +11,13 @@ int evaluate(string s,int i,int j,bool isTrue)
         else return s[i]=='F';
     }
 
-    if(dp[i][j]!= -1) return dp[i][j];
+    string temp = to_string(i);
+    temp.push_back(' ');
+    temp.append(to_string(j));
+    temp.push_back(' ');
+    temp.append(to_string(isTrue));
+    
+    if(mp.find(temp)!=mp.end()) return mp[temp];
 
     int ans=0;
 
@@ -40,12 +46,12 @@ int evaluate(string s,int i,int j,bool isTrue)
             else ans+= lf*rf ;
         }
     }
-    return dp[i][j]=ans;
+    return mp[temp]=ans;
 }
 
 int main()
 {   
-    memset(dp,-1,sizeof(dp));
+    mp.clear();
 
     string s;
     cout<<"Enter boolean expression"<<endl;
