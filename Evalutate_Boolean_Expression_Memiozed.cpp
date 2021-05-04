021915346cda44995c231be582130e3ecae48dfb
Ayon Sen <ayon.s2000@gmail.com>
2021-05-04 18:59:17 +0530

    Evaluate Boolean Expression Memoized

---
+++ Evalutate_Boolean_Expression_Memiozed.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+int dp[1000][1000];
+
+int evaluate(string s,int i,int j,bool isTrue)
+{
+    if(i>j) return false;
+    if(i==j)
+    {
+        if(isTrue) return s[i]=='T';
+        else return s[i]=='F';
+    }
+
+    if(dp[i][j]!= -1) return dp[i][j];
+
+    int ans=0;
+
+    for (int k = i+1; k <j ; k+=2)
+    {
+        int lt = evaluate(s,i,k-1,true);
+        int lf = evaluate(s,i,k-1,false);
+        int rt = evaluate(s,k+1,j,true);
+        int rf = evaluate(s,k+1,j,false);
+
+
+        if(s[k]=='^')
+        {
+
+            if(isTrue) ans+= lt*rf+rt*lf;
+             else ans+= lt*rt+rf*lf;    
+        }
+        if(s[k]=='&')
+        {
+            if(isTrue) ans+=lt*rt;
+            else ans+= lt*rf+lf*rt+lf*rf ;
+        }
+        if(s[k]=='|')
+        {
+            if(isTrue) ans+=lt*rt+lt*rf+rt*lf;
+            else ans+= lf*rf ;
+        }
+    }
+    return dp[i][j]=ans;
+}
+
+int main()
+{   
+    memset(dp,-1,sizeof(dp));
+
+    string s;
+    cout<<"Enter boolean expression"<<endl;
+    cin>>s;
+
+    int ans=evaluate(s,0,s.length()-1,true);
+
+    cout<<"Number of ways to evaluate the given expression to true is "<<ans<<endl;
+
+}
