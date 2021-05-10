9a5f64457cad51892e1d1a6ee03778bac8b0e0e7
Ayon Sen <ayon.s2000@gmail.com>
2021-05-10 17:39:07 +0530

    Egg_Dropping_Memoized.cpp

---
+++ Egg_Dropping_Memoized.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+int dp[1000][1000];
+
+int egg_drop(int e,int f)
+{
+    if(e==1) return f;
+
+    if(f==0 or f== 1) return f;
+
+    if(dp[e][f]!= -1) return dp[e][f];
+
+    int mn = INT_MAX;
+
+    for (int k = 1; k <= f; k++)
+    {
+        int temp = 1 + max(egg_drop(e-1,k-1),egg_drop(e,f-k));
+        mn = min (mn,temp);    
+    }
+
+    return dp[e][f] = mn;
+    
+}
+int main()
+{
+    memset(dp,-1,sizeof(dp));
+
+    int e,f;
+
+    cout<<"Enter number of eggs"<<endl;
+    cin>>e;
+
+    cout<<"Enter number of floors"<<endl;
+    cin>>f;
+
+    int ans = egg_drop(e,f);
+
+    cout<<"The minimum number of attempts to find critical floor in worst case is "<<ans<<endl;
+
+}
